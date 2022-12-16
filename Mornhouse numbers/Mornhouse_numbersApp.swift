582447a54b8eac7a5ea33ff4100cd407578e49e3
ibohonos <The.head1993@gmail.com>
2022-12-16 16:01:27 +0200

    Initial Commit

---
+++ Mornhouse numbers/Mornhouse_numbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mornhouse_numbersApp.swift
+//  Mornhouse numbers
+//
+//  Created by Іван Богоносюк on 16.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Mornhouse_numbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
